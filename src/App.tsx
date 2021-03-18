39926a66a8584e655cda820ad90162c922376e14
simone98dm <simone.dalmas@outlook.it>
2021-03-18 22:35:14 +0100

    Add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Kanban from './components/Kanban/Kanban';
+import { ICard } from './models/Card';
+import { IKanban } from './models/Kanban';
+import { IList } from './models/List';
 
-function App() {
+const App = () => {
+  const todoList: ICard[] = [
+    { title: 'Create layout', description: '' },
+    { title: 'Create project', description: '' },
+    { title: 'Deploy in heroku', description: '' },
+    { title: 'Hack the world', description: '' }
+  ];
+  const progressList: ICard[] = [
+    { title: 'Completing this project', description: '' },
+    { title: 'print "Hello, World!"', description: '' }
+  ];
+  const toCheckList: ICard[] = [
+    { title: 'The water on the gas', description: '' },
+    { title: 'Did I close the main door?', description: '' }
+  ];
+  const completedList: ICard[] = [
+    { title: "Have an idea!", description: '' }
+  ];
+
+  const lists: IList[] = [{
+    name: 'Todo',
+    list: todoList
+  }, {
+    name: 'Progress',
+    list: progressList
+  }, {
+    name: 'To Check',
+    list: toCheckList
+  }, {
+    name: 'Completed',
+    list: completedList
+  }]
+
+  const kanban: IKanban = {
+    name: 'Main',
+    lists: lists
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Kanban data={kanban} />
     </div>
   );
 }
